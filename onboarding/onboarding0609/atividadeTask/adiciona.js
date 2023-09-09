e4536557a5d0719a9634ba425e883f350d339e70
naralicecosta <naralicecosta04@gmail.com>
2023-09-09 14:50:13 -0300

    criando evento de excluir e editar tarefa

--- onboarding/onboarding0609/atividadeTask/adiciona.js
+++ onboarding/onboarding0609/atividadeTask/adiciona.js
@@ -1,4 +1,4 @@
-function adiciona() {
+/*function adiciona() {
   const tarefasfazer = document.getElementById('tarefasfazer').value;
   let regexTesteTarefas = /\S+/;
   var listDeTarefas = document.getElementById('listaDeTarefas');
@@ -18,31 +18,90 @@ function adiciona() {
     // Limpar o campo de entrada
     document.getElementById('tarefasfazer').value = '';
   } else {
-    alert('Tarefa já existe');
+    alert('Tarefa inválida ou já existe');
   }
 }
 
+  // Criar um novo elemento de lista (li) com botões de edição/exclusão
+var novaTarefa = document.createElement('li');
+novaTarefa.innerHTML =  `${novaTarefa}
+<button class="btnAcao">
+  <i class="fa fa-pencil"></i>
+</button>
+<button class="btnAcao">
+  <i class="fa fa-trash"></i>
+</button>
+`;
 
+  // Adicionar o novo elemento à lista de tarefas
+listaDeTarefas.appendChild(novaTarefa);
+  // Limpar o campo de entrada
+tarefasfazer.value = ''
 
+//funcao para editar
+function editarTarefa(btnAcao){
+  const tarefaParaEditar = btnAcao.parentNode;
+  const tarefaTexto = tarefaParaEditar.firstChild.textContent;
+  const editIndexInput = document.getElementById('editIndex')
+  editIndexInput.value = tarefaTexto;
+  tarefaParaEditar.remove();
+  document.getElementById('tarefasfazer').value = tarefaTexto;
+}
+//// Função para excluir uma tarefa
+function excluirTarefa(btnAcao) {
+  const tarefaParaExcluir = btnAcao.parentNode;
+  tarefaParaExcluir.remove();
+}*/
 
+// Função para adicionar uma nova tarefa
+function adiciona() {
+  const tarefaInput = document.getElementById('tarefasfazer');
+  const tarefa = tarefaInput.value.trim();//.trim  retorna uma nova string com os espaços em branco removidos, mas não altera a string original.
 
+  //vai exibir uma mensagem se não digitar nada no campo
+  if (tarefa === '') {
+    alert('Por favor, insira uma tarefa antes de adicionar.');
+    return;
+  }
 
+  const listaDeTarefas = document.getElementById('listaDeTarefas');
 
-
-
-
-/*function adiciona() {
-    const tarefasfazer = document.getElementById('tarefasfazer').value
-    let regexTesteTarefas = /\S+@\S+\.\S+/
-    var listaDeTarefas = document.getElementById('tarefasfazer')
-  
-    var listaTarefasAdicionadas = listaTarefasAdicionadas.innerHTML.split('<li>')
-  
-    var existeTarefa = listaTarefasAdicionadas.includes(tarefasfazer+'</li>')
-  
-    if (regexTesteTarefas.test(tarefasfazer) && !existeTarefa) {
-      listaDeTarefas.innerHTML = listaDeTarefasinnerHTML + `<li>${listaDeTarefas}</li>`
-    } else {
-      alert('Tarefa inválida ou ja existe')
+  // Verifique se a tarefa já existe na lista
+  const tarefasNaLista = listaDeTarefas.querySelectorAll('li');
+  for (const tarefaNaLista of tarefasNaLista) {
+    if (tarefaNaLista.textContent === tarefa) {
+      alert('Esta tarefa já está na lista.');
+      return;
     }
-  }*/
+  }
+
+  // Criar um novo elemento de lista (li) com botões de edição/exclusão
+  const novaTarefa = document.createElement('li');
+  novaTarefa.innerHTML = `
+    ${tarefa}
+    <button class="btnAcao" onclick="editarTarefa(this)">Editar</button>
+    <button class="btnAcao" onclick="excluirTarefa(this)">Excluir</button>
+  `;
+
+  // Adicionar o novo elemento à lista de tarefas
+  listaDeTarefas.appendChild(novaTarefa);
+
+  // Limpar o campo de entrada
+  tarefaInput.value = '';
+}
+
+// Função para editar uma tarefa
+function editarTarefa(button) {
+  const tarefaParaEditar = button.parentNode;
+  const tarefaTexto = tarefaParaEditar.firstChild.textContent;
+  const editIndexInput = document.getElementById('editIndex');
+  editIndexInput.value = tarefaTexto; // Armazene o texto da tarefa no campo oculto
+  tarefaParaEditar.remove(); // Remova a tarefa da lista
+  document.getElementById('tarefasfazer').value = tarefaTexto; // Preencha o campo de entrada com a tarefa
+}
+
+// Função para excluir uma tarefa
+function excluirTarefa(button) {
+  const tarefaParaExcluir = button.parentNode;
+  tarefaParaExcluir.remove();
+}
